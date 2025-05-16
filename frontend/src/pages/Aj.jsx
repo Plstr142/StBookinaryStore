e00333ea3d644242097b39bdbd875c97f43f76ac
Phongs4thon <plstr142@gmail.com>
2025-05-16 19:52:47 +0700

    first commit

--- frontend/src/pages/Aj.jsx
+++ frontend/src/pages/Aj.jsx
@@ -6,6 +6,10 @@ function Aj() {
       <h1>Aj</h1>
       <h1>Ajasdasdsa</h1>
       <h1>Ajsadasdwsasadsadsa</h1>
+      <h1>Ajsadasdwsasadsadsa</h1>
+      <h1>Ajsadasdwsasadsadsa</h1>
+      <h1>Ajsadasdwsasadsadsa</h1>
+      <h1>Ajsadasdwsasadsadsa</h1>
     </>
   );
 }
