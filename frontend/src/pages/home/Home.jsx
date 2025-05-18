2da5699f2e67b52eb2e93ae1c1d19bdc844e1f5b
Phongs4thon <plstr142@gmail.com>
2025-05-18 14:54:51 +0700

    update

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSales from './TopSales'
+import UserNameInput from '../../components/UserNameInput'
 
 const Home = () => {
+
+
+  // console.log("Re-rendered")
+
+  
   return (
     // border border-yellow-400
     <>
       <Banner />
       <TopSales />
+      <UserNameInput />
     </>
   )
 }
