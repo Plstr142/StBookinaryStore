554733d3dbc814bd9dfac695104bb04732fc78d5
Phongs4thon <plstr142@gmail.com>
2025-05-20 21:32:24 +0700

    update routes

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import About from "../pages/About.jsx";
 import Contact from "../pages/Contact.jsx";
@@ -9,52 +9,30 @@ import Highlights from "../pages/Highlights.jsx";
 import Orders from "../pages/Orders.jsx";
 import Login from "../components/Login.jsx";
 import Register from "../components/Register.jsx";
+import { routeDefs } from "./routeDefs.js";
+import React from "react";
+
+const componentsMap = {
+  Home,
+  About,
+  Contact,
+  Orders,
+  Fiction,
+  NonFiction,
+  Highlights,
+  Login,
+  Register,
+};
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                path: "/",
-                // outline-1 outline-green-400
-                element: <Home />
-            },
-            {
-                path: "/about",
-                element: <About />
-
-            },
-            {
-                path: "/contact",
-                element: <Contact />
-            },
-            {
-                path: "/orders",
-                element: <Orders />
-            },
-            {
-                path: "/fiction",
-                element: <Fiction />
-            },
-            {
-                path: "/nonfiction",
-                element: <NonFiction />
-            },
-            {
-                path: "/hightlights",
-                element: <Highlights />
-            },
-            {
-                path: "/login",
-                element: <Login />
-            },
-            {
-                path: "/register",
-                element: <Register />
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <App />,
+    children: routeDefs.map((r) => ({
+      path: r.path,
+      element: React.createElement(componentsMap[r.componentName]),
+    })),
+  },
+]);
 
 export default router;
---
+++ frontend/src/routers/showRoutes.js
@@ -0,0 +1,3 @@
+import { routeDefs } from "./routeDefs.js";
+
+routeDefs.forEach((r) => console.log(`Frontend route: ${r.path}`));
