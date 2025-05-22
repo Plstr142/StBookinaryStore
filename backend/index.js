09dc7d8265e50a31c8d892588d547e867f531c87
Phongs4thon <plstr142@gmail.com>
2025-05-22 18:57:58 +0700

    update book routes & controllers

--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,45 @@
-// const express = require("express");
-// const app = express();
-
-// const mongoose = require("mongoose");
-
-// const port = process.env.PORT || 5000;
-
-// **** routes; ****
-// async function main() {
-//   await mongoose.connect("mongodb://127.0.0.1:27017/test");
-
-//   app.use("/", (req, res) => {
-//     res.send("StBookinary Server is running!");
-//   });
-
-//   app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`);
-//   });
-
-//   console.log("MongoDB connect successfully!");
-// }
-
-// main().catch((err) => console.log(err));
-
 const express = require("express");
+const app = express();
+const cors = require("cors");
+
 const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-const app = express();
-const port = process.env.PORT || 5000;
-
 // Connect to MongoDB
-// Password : Skqe68bgoE3EFzd8
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected successfully");
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connected successfully");
   } catch (err) {
-    console.error("❌ Failed to connect to MongoDB:", err);
+    console.error("Failed to connect to MongoDB:", err);
     process.exit(1);
   }
 }
 
 // Initialize server routes and start listening
 function initServer() {
+  // middleware
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: ["http://localhost:5173"],
+      credentials: true,
+    })
+  );
+
   // Middleware / Routes
-  app.use("/", (req, res) => {
-    res.send("📘 StBookinary Server is running!");
-  });
+  // app.use("/", (req, res) => {
+  //   res.send("StBookinary Server is running!");
+  // });
+
+  // routes
+  const bookRoutes = require("./src/books/book.route.js");
+  app.use("/api/books", bookRoutes);
 
   // Start server
   app.listen(port, () => {
-    console.log(`🚀 Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
   });
 }
 
