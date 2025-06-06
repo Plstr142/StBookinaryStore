58a546fa1d84c2eecf90c9554e3594e5164ac43d
Phongs4thon <plstr142@gmail.com>
2025-06-06 22:48:30 +0700

    update

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -86,47 +86,4 @@ export default router;
 
 
 
-// import React from "react";
-// import { createBrowserRouter } from "react-router-dom";
-// import App from "../App";
-// import About from "../pages/About.jsx";
-// import Contact from "../pages/Contact.jsx";
-// import Home from "../pages/home/Home.jsx";
-// import Fiction from "../pages/Fiction.jsx";
-// import NonFiction from "../pages/Nonfiction.jsx";
-// import Highlights from "../pages/Highlights.jsx";
-// import Orders from "../pages/Orders.jsx";
-// import Login from "../components/Login.jsx";
-// import Register from "../components/Register.jsx";
-// import CartPage from "../pages/books/CartPage.jsx";
-// import CheckOutPage from "../pages/books/CheckoutPage.jsx";
-// import { routeDefs } from "./showRoutes.js";
-// import SingleBook from "../pages/books/SingleBook.jsx";
 
-// const componentsMap = {
-//   Home,
-//   About,
-//   Contact,
-//   Orders,
-//   Fiction,
-//   NonFiction,
-//   Highlights,
-//   Login,
-//   Register,
-//   CartPage,
-//   CheckOutPage,
-//   SingleBook
-// };
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//     children: routeDefs.map((r) => ({
-//       path: r.path,
-//       element: React.createElement(componentsMap[r.componentName]),
-//     })),
-//   },
-// ]);
-
-// export default router;
---
+++ otherrouter.jsx
@@ -0,0 +1,114 @@
+// ************ route อีกแบบ ************
+
+// import React from "react";
+// import { createBrowserRouter } from "react-router-dom";
+// import App from "../App";
+// import About from "../pages/About.jsx";
+// import Contact from "../pages/Contact.jsx";
+// import Home from "../pages/home/Home.jsx";
+// import Fiction from "../pages/Fiction.jsx";
+// import NonFiction from "../pages/Nonfiction.jsx";
+// import Highlights from "../pages/Highlights.jsx";
+// import Orders from "../pages/Orders.jsx";
+// import Login from "../components/Login.jsx";
+// import Register from "../components/Register.jsx";
+// import CartPage from "../pages/books/CartPage.jsx";
+// import CheckOutPage from "../pages/books/CheckoutPage.jsx";
+// import SingleBook from "../pages/books/SingleBook.jsx";
+// import { routeDefs } from "./routeDefs.js";
+// import { showRoutes } from "./showRoutes.jsx";
+// import PrivateRoute from "./PrivateRoute.jsx";
+
+// const componentsMap = {
+//   Home, 
+//   About, 
+//   Contact,
+//   Orders,
+//   Fiction,
+//   NonFiction,
+//   Highlights,
+//   Login,
+//   Register,
+//   CartPage,
+//   CheckOutPage,
+//   SingleBook
+// };
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: routeDefs.map((r) => ({
+//       path: r.path,
+//       element: React.createElement(componentsMap[r.componentName]),
+//     })),
+//   },
+// ]);
+
+// export default router;
+
+// ************ route อีกแบบ ************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { createBrowserRouter } from "react-router-dom";
+// import App from "../App";
+// import About from "../pages/About.jsx";
+// import Contact from "../pages/Contact.jsx";
+// import Home from "../pages/home/Home.jsx";
+// import Fiction from "../pages/Fiction.jsx";
+// import NonFiction from "../pages/Nonfiction.jsx";
+// import Highlights from "../pages/Highlights.jsx";
+// import Orders from "../pages/Orders.jsx";
+// import Login from "../components/Login.jsx";
+// import Register from "../components/Register.jsx";
+// import CartPage from "../pages/books/CartPage.jsx";
+// import CheckOutPage from "../pages/books/CheckoutPage.jsx";
+// import { routeDefs } from "./showRoutes.js";
+// import SingleBook from "../pages/books/SingleBook.jsx";
+
+// const componentsMap = {
+//   Home,
+//   About,
+//   Contact,
+//   Orders,
+//   Fiction,
+//   NonFiction,
+//   Highlights,
+//   Login,
+//   Register,
+//   CartPage,
+//   CheckOutPage,
+//   SingleBook
+// };
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: routeDefs.map((r) => ({
+//       path: r.path,
+//       element: React.createElement(componentsMap[r.componentName]),
+//     })),
+//   },
+// ]);
+
+// export default router;
